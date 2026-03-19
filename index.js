56ce20fc7ecc403d2ea12dae2fa78f666490d590
Samuel Vega <samuel_kripton22@hotmail.com>
2026-03-18 23:39:55 -0300

    Mi primer API

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.disable("x-powered-by");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
